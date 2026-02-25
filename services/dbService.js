6ebdeb2ea94c9c60f08899ec0d04d592b8ef56f0
Princess Kari <princess.midori@yahoo.com>
2026-02-25 16:15:06 +0100

    added database Service

---
+++ services/dbService.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose');
+
+initialiseDatabase().catch(err => console.log(err));
+
+async function initialiseDatabase() {
+    await mongoose.connect(process.env.MONGO_URL);
+}
+
+const dbEntrySchema = new mongoose.Schema({
+    userId: { type: String, required: true, index: true },
+    username: String,
+    pokemon: Object,
+    location: String,
+    timestamp: { type: Date, default: Date.now }
+});
+
+const dbEntry = mongoose.model('dbEntry', dbEntrySchema);
+
+// Add new Pokemon as a database entry for a given user
+async function addUserPokemon(user, pokemonData, guildName) {
+    try {
+        const newDbEntry = new dbEntry({
+            userId: user.id,
+            username: user.username,
+            pokemon: pokemonData,
+            location: guildName || 'Direct Message',
+            timestamp: new Date()
+        });
+
+        await newDbEntry.save();
+        return newDbEntry;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Retrieve most recent Pokemon for a given user
+async function getUserRecentPokemon(userId) {
+    try {
+        const recentPokemon = await dbEntry.findOne({ userId })
+        .sort({ timestamp: -1 })
+        .lean();
+
+        return recentPokemon;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Retrieve all Pokemons for a given user
+async function getUserAllPokemons(userId) {
+    try {
+        return await dbEntry.find({ userId })
+        .sort({ timestamp: -1 })
+        .lean();
+    } catch (error) {
+        console.log(error);
+        return [];
+    }
+}
+
+
+module.exports = {
+    initialiseDatabase,
+    addUserPokemon,
+    getUserRecentPokemon,
+    getUserAllPokemons
+}
